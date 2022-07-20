cc41c50c9f6b2eb5cfc7f6057a23f054be94ed29
brian <bn3l10@gmail.com>
2022-07-20 03:04:34 -0400

    added bootstrap to html added css style

--- script.js
+++ script.js
@@ -10,3 +10,60 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
+//search box elements
+var citySearchEl = document.querySelector("#citySearch");
+var enterCityVal = document.querySelector("#enterCity");
+var citySearchBtn = document.querySelector("#citySearchBtn");
+var searchedCitiesEl = document.querySelector(".listYourCities")
+//open weather api
+
+var apiKey = "0f03dfce7a166afdf62967ae1a00af7e";
+
+function handleSearch(event) {
+    event.preventDefault();
+  
+    var enterCity = enterCityVal.value.trim();
+  
+    if (!enterCity) {
+      // alert empty input error to user
+      errorMessage("You must enter a valid city name", citySearchEl, 5000);
+      return;
+    } else {
+      getCurrentWeather(enterCity, apiKey);
+      getForecast(enterCity, apiKey);
+      enterCityVal.value = "";
+      weatherContentDiv.classList.add("hide");
+    }
+  }
+
+
+// Add eventListener to search button
+citySearchBtn.addEventListener("click", handleSearch);
+
+
+// function to create new list item in the sidebar with the city's name
+function createNewCityButton(cityName, location) {
+    var cityBtnEl = document.createElement("button");
+    cityBtnEl.setAttribute("type", "button");
+    cityBtnEl.classList.add("listYourCities-item", "listYourCities-item-action");
+    cityBtnEl.textContent = cityName;
+    cityBtnEl.setAttribute("value", cityName);
+    location.prepend(cityBtnEl);
+    cityBtnEl.addEventListener("click", function () {
+      // remove active class from other buttons
+      var allCityBtns = document.querySelectorAll(".listYourCities-item");
+      for (var i = 0; i < allCityBtns.length; i++) {
+        allCityBtns[i].classList.remove("active");
+      }
+      getCurrentWeather(cityBtnEl.value, apiKey);
+      getForecast(cityBtnEl.value, apiKey);
+      cityBtnEl.classList.add("active");
+    });
+  }
+
+
+
+
+
+
+
